9547241088ba98fea30d9351122b3e9e2db5a42c
n0eyes <doll4586633@naver.com>
2023-11-13 20:53:31 +0900

    feat: useForm 추가

---
+++ src/hooks/Form/useForm.ts
@@ -0,0 +1,179 @@
+import { ChangeEvent, FormEvent, RefCallback, useRef, useState } from 'react';
+import { createFormsStore } from '@/utils/formStore/createFormStore';
+
+import { invariantOf } from '@/utils/@common/invariantOf';
+import { ValueOf } from '@/utils/@common/utility';
+
+import type {
+  FieldName,
+  FormFields,
+  Store,
+  Validation,
+  InputValue,
+  Errors,
+} from '@/utils/formStore/createFormStore';
+
+interface UseFormOptions<DefaultValues extends FormFields> {
+  defaultValues?: DefaultValues;
+}
+
+const useFormStore = <DefaultValues extends FormFields = FormFields>(
+  options?: UseFormOptions<DefaultValues>,
+) => {
+  type FormStore = Store<DefaultValues>;
+  type FormErrors = Errors<DefaultValues>;
+  type Name = FieldName<DefaultValues>;
+
+  const formStore = useRef(createFormsStore<DefaultValues>(options?.defaultValues)).current;
+
+  const [snapshot, setSnapshot] = useState<FormStore>(formStore.store);
+
+  const [errors, setErrors] = useState<FormErrors>({} as FormErrors);
+
+  const setError = (name: Name, info?: ValueOf<FormErrors>) => {
+    setErrors(errors => ({
+      ...errors,
+      [name]: {
+        ...errors[name],
+        ...info,
+      },
+    }));
+  };
+
+  const deleteError = (name: Name) => {
+    if (errors[name]) {
+      setErrors(errors => {
+        const clonedErrors = structuredClone(errors);
+
+        delete clonedErrors[name];
+
+        return clonedErrors;
+      });
+    }
+  };
+
+  const takeSnapShot = (name: Name, value: unknown) => {
+    setSnapshot(prev => ({ ...prev, [name]: { ...prev[name], value } }));
+  };
+
+  return {
+    store: formStore,
+    snapshot,
+    errors,
+    setError,
+    takeSnapShot,
+    deleteError,
+  };
+};
+
+export const useForm = <DefaultValues extends FormFields>(
+  options?: UseFormOptions<DefaultValues>,
+) => {
+  type Name = FieldName<DefaultValues>;
+  type SubmitValue = { [Name in FieldName<DefaultValues>]: string };
+  type FormErrors = Errors<DefaultValues>;
+
+  const {
+    store: {
+      store,
+      registerField,
+      getFieldValue,
+      updateFieldValue,
+      watchField,
+      isWatching,
+      validateField,
+    },
+    errors,
+    snapshot,
+    setError,
+    deleteError,
+    takeSnapShot,
+  } = useFormStore(options);
+
+  const watch = <FieldNames extends Name[]>(
+    fieldsNames: FieldNames,
+  ): { [Name in FieldNames[number]]: string } => {
+    return fieldsNames.reduce((acc, name) => {
+      watchField(name);
+
+      return {
+        ...acc,
+        [name]: snapshot[name].value,
+      };
+    }, {} as any);
+  };
+
+  const register = (
+    name: Name,
+    options?: {
+      value?: InputValue;
+      validations?: Validation[];
+    },
+  ) => {
+    registerField(name, {
+      value: generateValueAttr(options?.value),
+      validations: options?.validations,
+    });
+
+    const onChange = ({ target: { value } }: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+      updateFieldValue(name, { value });
+
+      validateField({
+        name,
+        onValid: () => deleteError(name),
+        onInvalid: ({ type, message }) => !errors[name] && setError(name, { type, message }),
+      });
+
+      if (isWatching(name)) takeSnapShot(name, value);
+    };
+
+    const ref: RefCallback<HTMLInputElement | HTMLSelectElement> = instance => {
+      if (!instance) return;
+
+      instance.value = getFieldValue(name);
+    };
+
+    return {
+      name,
+      onChange,
+      ref,
+    };
+  };
+
+  const handleSubmit = (
+    onValid?: (value: SubmitValue, e: FormEvent<HTMLFormElement>) => void,
+    onInvalid?: (errors: FormErrors, e: FormEvent<HTMLFormElement>) => void,
+  ) => {
+    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      const formData = new FormData(e.currentTarget);
+      const formFields = Object.fromEntries(formData) as SubmitValue;
+
+      const isAllValid = Object.keys(invariantOf(formFields)).every(name => {
+        /**@todo schema check */
+
+        /**@todo use store errors */
+        return validateField({
+          name,
+          onValid: () => deleteError(name),
+          onInvalid: ({ type, message }) => setError(name, { type, message }),
+        });
+      });
+
+      isAllValid ? onValid?.(formFields, e) : onInvalid?.(errors, e);
+    };
+
+    return onSubmit;
+  };
+
+  return { store, errors, setError, register, watch, handleSubmit };
+};
+
+const generateValueAttr = (value: InputValue): string => {
+  if (!value) return '';
+
+  if (Array.isArray(value)) return value.join();
+
+  return String(value);
+};
