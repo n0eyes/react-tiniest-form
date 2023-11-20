d6e9a875abed76a65d08d379cd271eba19f17bcb
n0eyes <doll4586633@naver.com>
2023-11-20 17:04:59 +0900

    refactor: redirection을 위해 Redirect 컴포넌트 사용

--- website/src/pages/index.jsx
+++ website/src/pages/index.jsx
@@ -1,10 +1,11 @@
 import useIsBrowser from '@docusaurus/useIsBrowser';
+import { Redirect } from '@docusaurus/router';
 
 export default function Home() {
   const isBrowser = useIsBrowser();
 
   if (isBrowser) {
-    location.href = '/docs/category/시작하기';
+    return <Redirect to={`/react-tiniest-form/docs/category/시작하기`} />;
   }
 
   return null;
